5ab9cddc5aa2f65f508d95607ab0e77391f794cf
Cleiton Antonio Goncalves <cleiton-ag@hotmail.com>
2016-02-13 01:02:24 -0200

    Colocando Rodapé e estilo

--- Contato.php
+++ Contato.php
@@ -6,6 +6,7 @@
         <title>Site Simples em PHP</title>
         <link rel="stylesheet" href="css/bootstrap.css">
         <link rel="stylesheet" href="css/bootstrap-responsive.css">
+        <link rel="stylesheet" href="css/estilo.css">  
         <script type="text/javascript" src="js/bootstrap.js"></script>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="js/jquery-1.7.1.min.js"><\/script>')</script>
@@ -25,6 +26,7 @@
             
         </form>
          <script type="text/javascript" src="js/bootstrap.js"></script>
+         <?php include ("radape.php"); ?>
      </body>
    
 </html>
---
+++ radape.php
@@ -0,0 +1,9 @@
+<div class="footer">
+    "Todos os direitos reservados -
+<?php
+$ano_atual = date("Y");
+
+echo $ano_atual;
+
+?> "
+</div>
