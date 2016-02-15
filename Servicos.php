2cdb99e71e63766c034d6c4114757d48bb2b0452
Cleiton <cleiton-ag@hotmail.com>
2016-02-15 19:01:49 -0200

    Adicionando requiere cabecalho

--- Servicos.php
+++ Servicos.php
@@ -1,16 +1,5 @@
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="description" content="Site Simples em PHP" />
-        <meta name="viewport" content="width-device-width, initial-scale-1" />
-        <title>Site Simples em PHP</title>
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <link rel="stylesheet" href="css/bootstrap-responsive.css">
-        <link rel="stylesheet" href="css/estilo.css">
-        <script type="text/javascript" src="js/bootstrap.js"></script>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="js/jquery-1.7.1.min.js"><\/script>')</script>
-    </head>
+<?php require_once ("cabecalho.php") ?>
     
     <body>
         <?php require_once ("menu.php"); ?>
